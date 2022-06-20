d9b044813437ece9198fcb175902927ef555260a
adanabdillahi <adanabdillahi30@gmail.com>
2022-06-20 12:46:45 +0100

    gym getmapping completed

--- src/components/gyms/Gym.js
+++ src/components/gyms/Gym.js
@@ -1,8 +1,16 @@
 const Gym = ({gym}) => {
+
+    const gymTrainers = gym.trainers.map ((trainer) => {
+        return <p key={trainer.id}>{trainer.name}</p>
+    })
+
     return (
         <>
         <h1> {gym.name}</h1>
-
+        <h3> Gym Leader</h3>
+        <p>{gym.gymLeader.name}</p>
+        <h3> Trainers</h3>
+        {gymTrainers}
         </>
     );
 }
