ec5df056f41ce87933087fbe58f1b4a294b0bcdd
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-22 14:53:46 +0100

    Add wireframe for HomePage

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import pokemonLogo from '../images/pokemonLogo.png'
 
 const HomePage = () => {
   return (
-    // <div className="home_body">
-    //   <div className="home_body_logo">
-    //     <div className="home_body_main"> 
-    //     </div>
-    //   </div>
-    // </div>
+    <div className="home_body">
+      <figure className="home_body_logo"><img src={pokemonLogo} alt="Pokemon Logo"/> </figure>
+        <div className="home_body_main"> 
+          <article>
+            <h1>What is Pokemon?</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Nisl pretium fusce id velit. Mollis nunc sed id semper risus in hendrerit. Nec ultrices dui sapien eget mi proin sed libero. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Enim nulla aliquet porttitor lacus luctus accumsan. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus. Nulla aliquet porttitor lacus luctus. Nunc sed blandit libero volutpat. Hendrerit gravida rutrum quisque non. Quis varius quam quisque id diam vel quam. Nibh praesent tristique magna sit.
+          
+            Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Nunc sed blandit libero volutpat sed. Aliquam etiam erat velit scelerisque in dictum non consectetur a. Sagittis eu volutpat odio facilisis mauris sit amet massa vitae. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Tempor orci dapibus ultrices in iaculis nunc. Arcu cursus vitae congue mauris. Libero justo laoreet sit amet cursus sit amet dictum sit. Cras ornare arcu dui vivamus arcu felis. Sed augue lacus viverra vitae congue eu. Sapien faucibus et molestie ac feugiat sed. Amet tellus cras adipiscing enim eu turpis. Lacus luctus accumsan tortor posuere ac ut consequat semper. Ullamcorper dignissim cras tincidunt lobortis feugiat. Integer feugiat scelerisque varius morbi. Felis eget velit aliquet sagittis id consectetur.
 
+            Feugiat in ante metus dictum at tempor. Hendrerit dolor magna eget est lorem ipsum. Tempor nec feugiat nisl pretium fusce. Tincidunt eget nullam non nisi est sit amet facilisis magna. Turpis egestas integer eget aliquet nibh praesent. Nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Lacus sed viverra tellus in hac habitasse. Tellus in metus vulputate eu scelerisque felis. Lacus vel facilisis volutpat est. Adipiscing bibendum est ultricies integer quis auctor. Tincidunt eget nullam non nisi est sit amet. Ipsum dolor sit amet consectetur adipiscing elit duis tristique sollicitudin. Amet volutpat consequat mauris nunc congue nisi. Tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus. Aliquet sagittis id consectetur purus ut faucibus pulvinar. Eget felis eget nunc lobortis mattis aliquam. Enim neque volutpat ac tincidunt vitae semper. Non nisi est sit amet facilisis. Laoreet sit amet cursus sit amet dictum.
 
+            Ultrices neque ornare aenean euismod. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Sed arcu non odio euismod lacinia at quis risus sed. Odio tempor orci dapibus ultrices in. Fringilla phasellus faucibus scelerisque eleifend. Aenean sed adipiscing diam donec. Leo in vitae turpis massa sed elementum tempus egestas. Adipiscing elit duis tristique sollicitudin. Sed libero enim sed faucibus turpis in. Mi sit amet mauris commodo quis imperdiet massa tincidunt. In arcu cursus euismod quis viverra nibh. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Tellus at urna condimentum mattis. At imperdiet dui accumsan sit amet. Felis donec et odio pellentesque diam volutpat. Ac tortor dignissim convallis aenean et tortor at risus. Fusce ut placerat orci nulla. Ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Sodales neque sodales ut etiam sit amet nisl purus in. Nulla at volutpat diam ut venenatis tellus in metus vulputate.
+
+            Ornare lectus sit amet est placerat in. Euismod quis viverra nibh cras pulvinar mattis nunc. Proin sed libero enim sed faucibus turpis in eu mi. Proin sagittis nisl rhoncus mattis rhoncus urna. Porttitor rhoncus dolor purus non enim praesent. Diam sit amet nisl suscipit adipiscing bibendum. Risus pretium quam vulputate dignissim suspendisse in est ante in. Porttitor leo a diam sollicitudin. Pharetra sit amet aliquam id diam maecenas ultricies mi. Est velit egestas dui id ornare
+            </p>
+          </article>
+          <figure className="home_body_image"><img src='' alt="Pokemon"/> </figure>
+          <figure className="home_body_image"><img src='' alt="Pokemon"/> </figure>
+          <figure className="home_body_image"><img src='' alt="Pokemon"/> </figure>
+        </div>
+    </div>
   )
 }
 
