eb6b1ac02bcde9299c4c924889d25f2ca21f30ab
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-22 23:23:03 +0100

    Refactor if else statement to switch statement in Pokemon.js

--- src/components/pokemons/Pokemon.js
+++ src/components/pokemons/Pokemon.js
@@ -4,51 +4,63 @@ const Pokemon = ({pokemon}) => {
 
     let pokemonColorType = "";
 
-    if(pokemon.type === "FIRE") {
-        // console.log(pokemon.name);
-        pokemonColorType = "#FDDFDF";
-    }
-    else if (pokemon.type === "GRASS"){
-        pokemonColorType = "#DEFDE0";
-    }
-    else if (pokemon.type === "WATER"){
-        pokemonColorType = "#DEF3FD";
-    }
-    else if (pokemon.type === "GROUND"){
-        pokemonColorType = "#f4e7da";
-    }
-    else if (pokemon.type === "ROCK"){
-        pokemonColorType = "#d5d5d4";
-    }
-    else if (pokemon.type === "FAIRY"){
-        pokemonColorType = "#fceaff";
-    }
-    else if (pokemon.type === "POISON"){
-        pokemonColorType = "#98d7a5";
-    }
-    else if (pokemon.type === "BUG"){
-        pokemonColorType = "#f8d5a3";
-    }
-    else if (pokemon.type === "DRAGON"){
-        pokemonColorType = "#97b3e6";
-    }
-    else if (pokemon.type === "PSYCHIC"){
-        pokemonColorType = "#eaeda1";
-    }
-    else if (pokemon.type === "FLYING"){
-        pokemonColorType = "#F5F5F5";
-    }
-    else if (pokemon.type === "FIGHTING"){
-        pokemonColorType = "#E6E0D4";
-    }
-    else if (pokemon.type === "NORMAL"){
-        pokemonColorType = "#F5F5F5";
-    }
-    else if (pokemon.type === "STEEL") {
-        pokemonColorType = "#7b94a4";
-    }
-    else if (pokemon.type === "ELECTRIC") {
-        pokemonColorType = "#FCF7DE";
+    switch(pokemon.type) {
+        case "FIRE":
+            pokemonColorType = "#EE8130";
+            break;
+        case "GRASS":
+            pokemonColorType = "#7AC74C";
+            break;
+        case "WATER":
+            pokemonColorType = "#6390F0";
+            break;
+        case "GROUND":
+            pokemonColorType = "#E2BF65";
+            break;
+        case "ROCK":
+            pokemonColorType = "#B6A136";
+            break;
+        case "FAIRY":
+            pokemonColorType = "#D685AD";
+            break;
+        case "POISON":
+            pokemonColorType = "#A33EA1";
+            break; 
+        case "BUG":
+            pokemonColorType = "#A6B91A";
+            break; 
+        case "DRAGON":
+            pokemonColorType = "#6F35FC";
+            break; 
+        case "PSYCHIC":
+            pokemonColorType = "#F95587";
+            break; 
+        case "FLYING":
+            pokemonColorType = "#A98FF3";
+            break; 
+        case "FIGHTING":
+            pokemonColorType = "#C22E28";
+            break; 
+        case "NORMAL":
+            pokemonColorType = "#A8A77A";
+            break; 
+        case "STEEL":
+            pokemonColorType = "#B7B7CE";
+            break; 
+        case "ELECTRIC":
+            pokemonColorType = "#F7D02C";
+            break; 
+        case "ICE":
+            pokemonColorType = "#96D9D6";
+            break;
+        case "DARK":
+            pokemonColorType = "#705746";
+            break;
+        case "GHOST":
+            pokemonColorType = "#735797";
+            break;
+        default:
+            console.log("Switch cas error in Pokemon.js");
     }
 
     let [pokemonFrontSpriteURL, setPokemonFrontSpriteURL] = useState("");
@@ -62,7 +74,7 @@ const Pokemon = ({pokemon}) => {
     return(
         <div style={{backgroundColor: pokemonColorType}} className = "pokemon">
             <h1>{pokemon.name}</h1>
-            <img src={pokemonFrontSpriteURL} alt="A pokemon"/>
+            <img src={pokemonFrontSpriteURL} alt="A pokemon sprite"/>
             <h3>{pokemon.type}</h3>
             <h4>{pokemon.level}</h4>
         </div>
