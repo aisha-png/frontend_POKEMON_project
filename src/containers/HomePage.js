2ed2a50e507022ffd7b4c661216cf6bcdafaed8d
adanabdillahi <adanabdillahi30@gmail.com>
2022-06-22 14:44:30 +0100

    div tags

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    // <div className="home_body">
+    //   <div className="home_body_logo">
+    //     <div className="home_body_main"> 
+    //     </div>
+    //   </div>
+    // </div>
+
+
   )
 }
 
