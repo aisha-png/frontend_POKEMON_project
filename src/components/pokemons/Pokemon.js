3a0b46508f7b1744dea34ab64d239fe28518b6ec
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-22 10:34:14 +0100

    Remove redundant code from Pokemon.js

--- src/components/pokemons/Pokemon.js
+++ src/components/pokemons/Pokemon.js
@@ -1,26 +1,5 @@
 const Pokemon = ({pokemon}) => {
 
-    // const colors = {
-    //     fire: "#FDDFDF",
-    //     grass: "#DEFDE0",
-    //     electric: "#FCF7DE",
-    //     water: "#DEF3FD",
-    //     ground: "#f4e7da",
-    //     rock: "#d5d5d4",
-    //     fairy: "#fceaff",
-    //     poison: "#98d7a5",
-    //     bug: "#f8d5a3",
-    //     dragon: "#97b3e6",
-    //     psychic: "#eaeda1",
-    //     flying: "#F5F5F5",
-    //     fighting: "#E6E0D4",
-    //     normal: "#F5F5F5",
-    //   };
-    //   const main_types = Object.keys(colors);
-    //   const poke_types = pokemon.types.map((type) => type.type.name);
-    // const type = main_types.find((type) => poke_types.indexOf(type) > -1);
-    // const color = colors[type];
-
     let pokemonColorType = "";
 
     if(pokemon.type === "FIRE") {
