01224e3591225e6f8d47a38e164dbba71f65a37c
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-23 15:59:13 +0100

    Add gym leader images logic

--- src/components/gymLeaders/GymLeader.js
+++ src/components/gymLeaders/GymLeader.js
@@ -44,10 +44,82 @@ const GymLeader = ({gyms, gymLeader}) => {
                 break;
         case 'Blue':
             gymLeaderImageSource = image.blue;
-                break;
+            break;
+        case 'Roark':
+            gymLeaderImageSource = image.roark;
+            break;
+        case 'Gardenia':
+            gymLeaderImageSource = image.gardenia;
+            break;
+        case 'Maylene':
+            gymLeaderImageSource = image.maylene;
+            break;
+        case 'Crasher Wake':
+            gymLeaderImageSource = image.crasherWake;
+            break;
+        case 'Fantina':
+            gymLeaderImageSource = image.fantina;
+            break;
+        case 'Byron':
+            gymLeaderImageSource = image.byron;
+            break;
+        case 'Candice':
+            gymLeaderImageSource = image.candice;
+            break;
+        case 'Volkner':
+           gymLeaderImageSource = image.volkner;
+            break;
+        case 'Falkner':
+            gymLeaderImageSource = image.falkner;
+            break;
+        case 'Bugsy':
+            gymLeaderImageSource = image.bugsy;
+            break;
+        case 'Whitney':
+            gymLeaderImageSource = image.whitney;
+            break;
+        case 'Morty':
+            gymLeaderImageSource = image.morty;
+            break;
+        case 'Chuck':
+            gymLeaderImageSource = image.chuck;
+            break;
+        case 'Jasmine':
+            gymLeaderImageSource = image.jasmine;
+            break;
+        case 'Pryce':
+            gymLeaderImageSource = image.pryce;
+            break;
+        case 'Clair':
+            gymLeaderImageSource = image.clair;
+            break;
+        case 'Roxanne':
+            gymLeaderImageSource = image.roxanne;
+            break;
+        case 'Brawly':
+            gymLeaderImageSource = image.brawly;
+            break;
+        case 'Wattson':
+            gymLeaderImageSource = image.wattson;
+            break;
+        case 'Flannery':
+            gymLeaderImageSource = image.flannery;
+            break;
+        case 'Norman':
+            gymLeaderImageSource = image.norman;
+            break;
+        case 'Winona':
+            gymLeaderImageSource = image.winona;
+            break;
+        case 'Tate And Liza':
+            gymLeaderImageSource = image.tateAndLiza;
+            break;
+        case 'Wallace':
+            gymLeaderImageSource = image.wallace;
+            break;
         default:
             console.log("Issue in gym leader images...");  
-    }
+    }  
 
     return(
         <div className="gymLeader">
--- src/gymLeaderImages/gymLeaderImages.js
+++ src/gymLeaderImages/gymLeaderImages.js
@@ -6,6 +6,30 @@ import koga from '../gymLeaderImages/koga.png';
 import sabrina from '../gymLeaderImages/sabrina.png';
 import blaine from '../gymLeaderImages/blaine.png';
 import blue from '../gymLeaderImages/blue.png';
+import roark from '../gymLeaderImages/roark.png';
+import gardenia from '../gymLeaderImages/gardenia.png';
+import maylene from '../gymLeaderImages/maylene.png';
+import crasherWake from '../gymLeaderImages/crasherWake.png';
+import fantina from '../gymLeaderImages/fantina.png';
+import byron from '../gymLeaderImages/byron.png';
+import candice from '../gymLeaderImages/candice.png';
+import volkner from '../gymLeaderImages/volkner.png';
+import falkner from '../gymLeaderImages/falkner.png';
+import bugsy from '../gymLeaderImages/bugsy.png';
+import whitney from '../gymLeaderImages/whitney.png';
+import morty from '../gymLeaderImages/morty.png';
+import chuck from '../gymLeaderImages/chuck.png';
+import jasmine from '../gymLeaderImages/jasmine.png';
+import pryce from '../gymLeaderImages/pryce.png';
+import clair from '../gymLeaderImages/clair.png';
+import roxanne from '../gymLeaderImages/roxanne.png';
+import brawly from '../gymLeaderImages/brawly.png';
+import wattson from '../gymLeaderImages/wattson.png';
+import flannery from '../gymLeaderImages/flannery.png';
+import norman from '../gymLeaderImages/norman.png';
+import winona from '../gymLeaderImages/winona.png';
+import tateAndLiza from '../gymLeaderImages/tateAndLiza.png';
+import wallace from '../gymLeaderImages/wallace.png';
 
 const images = {
   brock,
@@ -15,7 +39,31 @@ const images = {
   koga,
   sabrina,
   blaine,
-  blue
+  blue,
+  roark,
+  gardenia,
+  maylene,
+  crasherWake,
+  fantina,
+  byron,
+  candice,
+  volkner,
+  falkner,
+  bugsy,
+  whitney,
+  morty,
+  chuck,
+  jasmine,
+  pryce,
+  clair,
+  roxanne,
+  brawly,
+  wattson,
+  flannery,
+  norman,
+  winona,
+  tateAndLiza,
+  wallace
 };
 
 export default images;
