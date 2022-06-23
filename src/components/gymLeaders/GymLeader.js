c947e3474ab4289f23e3d3beb38969f17514cbda
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-23 14:52:11 +0100

    Add gym leader image logic

--- src/components/gymLeaders/GymLeader.js
+++ src/components/gymLeaders/GymLeader.js
@@ -1,3 +1,5 @@
+import image from '../../gymLeaderImages/gymLeaderImages.js';
+
 const GymLeader = ({gyms, gymLeader}) => {
 
     // const matchedGymtoGymLeader = gyms.map(gym => {
@@ -16,11 +18,24 @@ const GymLeader = ({gyms, gymLeader}) => {
         return null;
     })
 
+    let gymLeaderImageSource = null;
+
+    if(gymLeader.name === 'Brock') {
+        gymLeaderImageSource = image.brock
+    } else if(gymLeader.name === 'Misty') {
+        gymLeaderImageSource = image.misty
+    } else if(gymLeader.name === 'Lt Surge') {
+        gymLeaderImageSource = image.ltsurge
+    } else if(gymLeader.name === 'Erika') {
+        gymLeaderImageSource = image.erika
+    }
+
     return(
         <div className="gymLeader">
         <h1>{gymLeader.name}</h1>
         {/* <h2>{gymLeader.gym.name}</h2> */}
         {matchedGymtoGymLeader}
+        <img src={gymLeaderImageSource} alt='lll'/>
         </div>
     );
 }
---
+++ src/gymLeaderImages/gymLeaderImages.js
@@ -0,0 +1,13 @@
+import brock from '../gymLeaderImages/brock.png';
+import misty from '../gymLeaderImages/misty.png';
+import ltsurge from '../gymLeaderImages/ltsurge.png';
+import erika from '../gymLeaderImages/erika.png';
+
+const images = {
+  brock,
+  misty,
+  ltsurge,
+  erika
+};
+
+export default images;
